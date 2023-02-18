3b20ea656cc4ba13a8e20c079210e25a72caad36
ConnerS <conner.sommmerfield@gmail.com>
2023-02-17 22:54:46 -0800

    unique complete

--- unique.c
+++ unique.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
-#define MAX_LENGTH 100
+#define WORD_LENGTH 50
+
+/* Program Flow */
+int nextWordExists(char buffer[]);
+int isUnique(char word[], char wordList[WORD_LENGTH][WORD_LENGTH], int count);
+int appendWord(char word[], char wordList[WORD_LENGTH][WORD_LENGTH], int count);
+void printWords(char wordList[WORD_LENGTH][WORD_LENGTH], int count);
+
+int main() {
+
+    int wordCount = 0;
+    char uniqueWords[WORD_LENGTH][WORD_LENGTH];     // Empty list that will store words
+    char buffer[WORD_LENGTH];                       // Will temporarily store each word
+
+    while (nextWordExists(buffer))                  // Read next word from input into buffer
+    {
+        if (isUnique(buffer, uniqueWords, wordCount))
+        {
+            wordCount = appendWord(buffer, uniqueWords, wordCount);
+        }            
+    }
+    
+    printWords(uniqueWords, wordCount);
+    
+    return 0;
+}
+
+/* If stdin has more input, place it into the buffer */
+int nextWordExists(char buffer[])
+{
+    return (fscanf(stdin, "%s", buffer) != EOF) ? 1:0;
+}
 
 
-int isUnique(char word[], char wordList[MAX_LENGTH][MAX_LENGTH], int count);
-int appendWord(char word[], char wordList[MAX_LENGTH][MAX_LENGTH], int count);
-
-
-int isUnique(char word[], char wordList[MAX_LENGTH][MAX_LENGTH], int count)
+/* Compares current word to every word in our list and return 0 if it finds a match */
+int isUnique(char word[], char wordList[WORD_LENGTH][WORD_LENGTH], int count)
 {
     int i;
     int j;
-    for (i = 0; i < count; i++)
+    for (i = 0; i < count; i++)         // For each words in list
     {
         j = 0;
-        while (wordList[i][j] == word[j])
+        while (wordList[i][j] == word[j])      // compare each letter of word in list and word in question
         {
-            if (word[j] == '\0') {
+            if (word[j] == '\0') {             // if null character reached we know words are equal, not unique
                 return 0;
             }
         j++;
@@ -24,38 +52,25 @@ int isUnique(char word[], char wordList[MAX_LENGTH][MAX_LENGTH], int count)
     return 1;
 }
 
-int appendWord(char word[], char wordList[MAX_LENGTH][MAX_LENGTH], int count)
+/* Adds our unique word to the list and returns the new number of words in the list (previous + 1) */
+int appendWord(char word[], char wordList[WORD_LENGTH][WORD_LENGTH], int count)
 {
     int j = 0;
-    while (word[j] != '\0') {
-        wordList[count][j] = word[j];
+    while (word[j] != '\0')
+    {
+        wordList[count][j] = word[j];      // Set letters of current word in list to letters of word 
         j++;
     }
     wordList[count][j] = '\0';
-    count++;
-    return count;
+    count++;                               // Needed so we can differentiate between strings
+    return count;                          // Number of words in list increased by 1
 }
 
-int main() {
-
-    int wordCount = 0;
-    char uniqueWords[MAX_LENGTH][MAX_LENGTH]; /* array to store uniqueWords */
-    
-    
-    char word[MAX_LENGTH];                      /* Buffer to store next word */
-    while (fscanf(stdin, "%s", word) != EOF) 
+/* Simple helper to print words of list */
+void printWords(char wordList[WORD_LENGTH][WORD_LENGTH], int count)
+{
+    for (int i = 0; i < count; i++) 
     {
-        if (isUnique(word, uniqueWords, wordCount))
-        {
-            wordCount = appendWord(word, uniqueWords, wordCount);
-        }            
+        fprintf(stdout, "%s ", wordList[i]); 
     }
-    
-    // print unique uniqueWords
-    for (int i = 0; i < wordCount; i++) 
-    {
-        fprintf(stdout, "%s ", uniqueWords[i]);
-    }
-    
-    return 0;
 }
